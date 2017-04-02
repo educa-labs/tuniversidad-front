d6c93e4528c86b171720697f8cb0638978a5796e
Ivan Wolf <ivanwolf15@gmail.com>
2017-04-02 18:52:46 -0300

    starting careers

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from '../containers/App';
 import Home from '../containers/Home';
+import Careers from '../containers/Careers';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="home" component={Home} />
+    <Route path="careers" component={Careers} />
   </Route>
 );
