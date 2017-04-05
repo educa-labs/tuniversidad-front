15dd2a9b0d168355dd0e4a0901c0ffdf2e43e6f3
Ivan Wolf <ivanwolf15@gmail.com>
2017-04-05 00:55:49 -0300

    informacion general agregada

--- src/components/CareerView/General.js
+++ src/components/CareerView/General.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import ResultCard from '../ResultCard';
 
 function General(props) {
   return (
     <div className="general-info">
       <ResultCard career={props.career} general />
+      <div className="description">
+        {props.career.info.description}
+      </div>
     </div>
   );
 }
+
+General.propTypes = {
+  career: PropTypes.object.isRequired,
+};
 
 export default General;
