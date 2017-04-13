02859b446fc582b32f3754b82ca0720581cd31bb
Ivan Wolf <ivanwolf15@gmail.com>
2017-04-13 16:26:35 -0300

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,58 @@
 import React, { PropTypes, Component } from 'react';
 import Dialog from 'material-ui/Dialog';
 import { connect } from 'react-redux';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 import { toggleShowLogin } from '../actions/compress';
+import '../styles/Login.css';
+
+const modalStyle = {
+  width: '25rem',
+};
+
+const buttonStyle = {
+  margin: '1rem 0',
+};
 
 class Login extends Component {
+  componentWillMount() {
+    this.setState({
+      email: '',
+      password: '',
+    });
+  }
+
   render() {
     return (
       <Dialog
-        title="Hola"
+        title="Inicia sesion en Tuniversidad"
         open={this.props.open}
         contentClassName="login-modal"
         titleClassName="login-title"
         onRequestClose={this.props.toggleShowLogin}
+        contentStyle={modalStyle}
       >
-        Hola amigos somos el login
+        <TextField
+          hintText="ivan@mail.com"
+          floatingLabelText="Correo electrónico"
+          onChange={(e, val) => this.setState({ email: val })}
+        />
+        <br />
+        <TextField
+          hintText="* * * * * *"
+          floatingLabelText="Contraseña"
+          type="password"
+          onChange={(e, val) => this.setState({ password: val })}
+        />
+        <br />
+        <RaisedButton
+          label="Inicia Sesión"
+          fullWidth
+          backgroundColor="#0091EA"
+          labelColor="#FFFFFF"
+          style={buttonStyle}
+        />
+        <div className="forgot">¿Olvidaste tu contraseña?</div>
       </Dialog>
     );
   }
