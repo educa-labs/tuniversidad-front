912b810229cd90b0dda22765a0c44d121bc73054
Ivan Wolf <ivanwolf15@gmail.com>
2017-04-21 17:11:39 -0300

    register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import IconButton from 'material-ui/IconButton';
+import Eye from 'material-ui/svg-icons/action/visibility';
+import NoEye from 'material-ui/svg-icons/action/visibility-off';
+import is from 'is_js';
 import '../styles/Register.css';
+import { signUser } from '../actions/user';
 
 class Register extends Component {
   componentWillMount() {
@@ -11,6 +16,7 @@ class Register extends Component {
       email: '',
       accept: false,
       showTerms: false,
+      showPass: false,
     });
     this.toggleAccept = this.toggleAccept.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -21,39 +27,51 @@ class Register extends Component {
   }
 
   handleSubmit() {
-    const { firstName, lastName, password, email, accept } = this.state;
-    console.log({ firstName, lastName, password, email, accept });
+    const { firstName, lastName, password, email } = this.state;
+    signUser(firstName, lastName, email, password);
   }
 
   render() {
-    const { firstName, lastName, password, email, accept } = this.state;
+    const { firstName, lastName, password, email, accept, showPass } = this.state;
+    const disabled = is.any.empty(firstName, lastName, password, email) || !accept;
     return (
       <div className="register-form" >
         <div className="row">
-          <input
-            value={firstName}
-            onChange={e => this.setState({ firstName: e.target.value })}
-            placeholder="Nombre"
-          />
-          <input
-            value={lastName}
-            onChange={e => this.setState({ lastName: e.target.value })}
-            placeholder="Apellido"
-          />
+          <div className="item">
+            <input
+              value={firstName}
+              onChange={e => this.setState({ firstName: e.target.value })}
+              placeholder="Nombre"
+            />
+          </div>
+          <div className="item">
+            <input
+              value={lastName}
+              onChange={e => this.setState({ lastName: e.target.value })}
+              placeholder="Apellido"
+            />
+          </div>
         </div>
         <div className="row">
-          <input
-            type="email"
-            value={email}
-            onChange={e => this.setState({ email: e.target.value })}
-            placeholder="Correo electrónico"
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={e => this.setState({ password: e.target.value })}
-            placeholder="Contraseña"
-          />
+          <div className="item ">
+            <input
+              type="email"
+              value={email}
+              onChange={e => this.setState({ email: e.target.value })}
+              placeholder="Correo electrónico"
+            />
+          </div>
+          <div className="item password">
+            <input
+              type={`${showPass ? '' : 'password'}`}
+              value={password}
+              onChange={e => this.setState({ password: e.target.value })}
+              placeholder="Contraseña"
+            />
+            <IconButton onTouchTap={() => this.setState({ showPass: !showPass })} >
+              {showPass ? <NoEye color="#E5E5E5" /> : <Eye color="#E5E5E5" />}
+            </IconButton >
+          </div>
         </div>
         <div className="terms">
           <input id="terms" type="checkbox" checked={accept} onClick={this.toggleAccept} />
@@ -65,6 +83,7 @@ class Register extends Component {
           backgroundColor="#0091EA"
           labelColor="#FFFFFF"
           fullWidth
+          disabled={disabled}
           onTouchTap={this.handleSubmit}
         />
       </div>
