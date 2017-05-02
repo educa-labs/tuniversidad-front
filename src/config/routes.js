4dc88892d73f72564b280f4fedf6bd4b716a1759
Ivan Wolf <ivanwolf15@gmail.com>
2017-05-02 18:01:28 -0300

    refactor home to landing

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,13 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from '../containers/App';
 import CareerView from '../containers/CareerView';
-import Home from '../containers/Home';
+import Landing from '../containers/Landing';
 import Buscador from '../containers/Buscador';
 import Compare from '../containers/Compare';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={Home} />
+    <IndexRoute component={Landing} />
     <Route path="search" component={Buscador} />
     <Route path="compare" component={Compare} />
     <Route path="careers/:id" component={CareerView} />
