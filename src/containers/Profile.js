a4f7bf14a3e7bf15e0cec94b586178271fc1c2bf
Ivan Wolf <ivanwolf15@gmail.com>
2017-05-25 18:45:09 -0400

    profile skeleton

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
+import { Tabs, Tab } from 'material-ui/Tabs';
+import '../styles/Profile.css';
 
-function Profile() {
-  return (
-    <div>
-      somos el perfil
-    </div>
-  );
+class Profile extends Component {
+  componentWillMount() {
+    this.setState({ slideIndex: 0 });
+  }
+
+  render() {
+    const { slideIndex } = this.state;
+    return (
+      <div className="site__children">
+        <div className="profile-cover">
+          <div className="profile-cover__title">Mi Perfil</div>
+        </div>
+        <div className="tabs-container">
+            <Tabs
+              onChange={this.handleSlideChange}
+              value={slideIndex}
+              className="tabs"
+            >
+              <Tab label="General" value={0} />
+              <Tab label="Progreso" value={1} />
+              <Tab label="Recomendaciones" value={2} />
+            </Tabs >
+          </div>
+      </div>
+    );
+  }
 }
 
 export default Profile;
