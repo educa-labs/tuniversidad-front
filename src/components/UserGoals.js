90addba2a94313f109c8ada85a7b6861ae85a928
Ivan Wolf <ivanwolf15@gmail.com>
2017-05-27 15:40:03 -0400

    added static barchart

--- src/components/UserGoals.js
+++ src/components/UserGoals.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import EditIcon from 'material-ui/svg-icons/image/edit';
 import IconButton from 'material-ui/IconButton';
-import { BarChart, Bar, Tooltip, XAxis, YAxis } from 'recharts';
+import BarChart from './BarChart';
 
-
-const data = [
-  { name: 'Yo', value: 623 },
-  { name: 'Mi meta', value: 700 },
-  { name: 'Corte 2016', value: 683 },
-];
 
 function UserGoals({ goals }) {
   function renderGoal(goal) {
     return (
-      <div className="general-card__goal" key={goal.id}>
-        {goal.title}
+      <div className="goal" key={goal.id}>
+        <div className="goal__title">{`${goal.title} en ${goal.university_name}`}</div>
+        <BarChart max={850} />
       </div>
     );
   }
@@ -28,17 +23,6 @@ function UserGoals({ goals }) {
           <IconButton><EditIcon color="#424242" /></IconButton>
         </div>
       </div>
-      <BarChart
-        width={400}
-        height={200}
-        data={data}
-        layout="vertical"
-        barGap={1}
-       >
-        <XAxis type="number" />
-        <Tooltip />
-        <Bar dataKey="value" fill="#0091EA" label />
-      </BarChart>
       {goals.map(goal => renderGoal(goal))}
     </div>
   );
