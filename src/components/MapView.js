fcbee5f00d46094f8037aae62a33f1b2da175240
Ivan Wolf <ivanwolf15@gmail.com>
2017-07-25 22:37:01 -0400

    mv

---
+++ src/components/MapView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import {GoogleApiWrapper} from 'google-maps-react';
+
+function MapView() {
+  return (
+    <div className="general-card">
+      Hola
+    </div>
+  );
+}
+
+export default MapView;
