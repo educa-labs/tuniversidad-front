6ba9a0ffafbc2ac0be07846db46a7b28200dd5cd
Ivan Wolf <ivanwolf15@gmail.com>
2017-08-01 19:41:53 -0400

    link to google play

--- src/components/landing/CoverBottom.js
+++ src/components/landing/CoverBottom.js
@@ -3,11 +3,11 @@ import React from 'react';
 function CoverBottom(props) {
   return (
     <div className="landing-cover-bottom">
-      <div className={`landing-body-title${props.mobile ? '-mobile' : ''} color-white`}>App disponible en Julio
+      <div className={`landing-body-title${props.mobile ? '-mobile' : ''} color-white`}>App disponible en Google Play
       </div>
       <div className="row">
-        <div className="app-store" />
-        <div className="google-play" />
+        <a className="app-store" href="#" />
+        <a className="google-play" href="https://play.google.com/store/apps/details?id=cl.tuniversidad" />
       </div>
     </div>
   );
