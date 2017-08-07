ae61351760f0869f1acdca655494ee4f68e7fd33
Ivan Wolf <ivanwolf15@gmail.com>
2017-08-07 17:46:17 -0400

    issue #54 nuevo diseño en mobile

--- src/containers/University.js
+++ src/containers/University.js
@@ -12,6 +12,8 @@ import CareerHeading from '../components/CareerHeading';
 import Loading from '../components/Loading';
 import { fetch } from '../actions/fetch';
 import '../styles/University.css';
+import { numeral } from '../helpers/numeral';
+import { getDate } from '../helpers/strings';
 
 
 const tabStyle = {
@@ -42,9 +44,56 @@ class University extends Component {
   }
 
   getContent(slideIndex) {
+    const { mobile, university } = this.props;
     switch (slideIndex) {
       case 0:
-        return (
+        return mobile ? (
+          <div>
+            <div className="career-section-header">Información</div>
+            <div className="career-section-body">
+              <div className="row">
+                <div className="expandible-label">Tipo</div>
+                <div className="expandible-value">{university.u_type}</div>
+              </div>
+              <div className="row">
+                <div className="expandible-label">Sigla</div>
+                <div className="expandible-value">{university.initials}</div>
+              </div>
+              <div className="row">
+                <div className="expandible-label">Gratuidad</div>
+                <div className="expandible-value">{university.freeness ? 'Sí' : 'No'}</div>
+              </div>
+              <div className="row">
+                <div className="expandible-label">Fundación</div>
+                <div className="expandible-value">{getDate(university.foundation)}</div>
+              </div>
+              <div className="row">
+                <div className="expandible-label">Alumnos</div>
+                <div className="expandible-value">{university.students ? numeral(university.students) : 'No disponible'}</div>
+              </div>
+              <div className="row">
+                <div className="expandible-label">Profesores</div>
+                <div className="expandible-value">{university.teachers ? numeral(university.teachers) : 'No disponible'}</div>
+              </div>
+              <div className="row">
+                <div className="expandible-label">Grados</div>
+                <div className="expandible-value">{university.degrees ? numeral(university.degrees) : 'No disponible'}</div>
+              </div>
+              <div className="row">
+                <div className="expandible-label">Postgrados</div>
+                <div className="expandible-value">{university.postgraduates ? numeral(university.postgraduates) : 'No disponible'}</div>
+              </div>
+              <div className="row">
+                <div className="expandible-label">Doctorados</div>
+                <div className="expandible-value">{university.doctorates ? numeral(university.doctorates) : 'No disponible'}</div>
+              </div>
+            </div>
+            <div className="career-section-header">Descripción</div>
+            <div className="career-section-body career-description">
+              {university.description}
+            </div>
+          </div>
+        ) : (
           <div>
             <UniversityCard
               university={this.props.university}
