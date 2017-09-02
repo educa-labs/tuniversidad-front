24cf8db8f8c1f687ef3d17219fcf1b141b049b5c
Ivan Wolf <ivanwolf15@gmail.com>
2017-09-02 14:10:38 -0300

    deleted unused conts

--- src/constants/num.js
+++ src/constants/num.js
@@ -4,5 +4,3 @@ export const MIN_PRICE = 0;
 export const MAX_PRICE = 7000000;
 export const MIN_CUT = 450;
 export const MAX_CUT = 850;
-export const MIN_SEMESTERS = 1;
-export const MAX_SEMESTERS = 16;
