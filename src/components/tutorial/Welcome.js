1839bd587fe92e957b5dc632ea90edfad6d0f0dc
Ivan Wolf <ivanwolf15@gmail.com>
2017-09-10 16:29:48 -0300

    newton moved

--- src/components/tutorial/Welcome.js
+++ src/components/tutorial/Welcome.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Welcome = () => (
   <div className="slide">
-    <div className="slide-header">¡Hola, soy Newton!</div>
+    <div className="slide-header">¡Hola!</div>
     <div className="slide-body">
-      <div className="newton-welcome" />
-      <div className="slide">
-        <p>¡Bienvenido a tuniversidad! Antes de comenzar, te haré un par de preguntas para saber más de ti. </p>
+      <div className="slide-col">
+        <p>Gracias por ingresar a Tuniversidad, como equipo estamos muy agradecidos y quisieramos saber un poco más de ti.</p>
+        <div className="row justify-center">
+          <div className="newton-welcome" />
+        </div>
       </div>
     </div>
   </div>
