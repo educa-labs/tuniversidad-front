fbaa59cad3fd30a9be8c6900aa4bd0c21eef5018
Ivan Wolf <ivanwolf15@gmail.com>
2017-09-11 11:14:36 -0300

    ready slide fixed

--- src/components/tutorial/Ready.js
+++ src/components/tutorial/Ready.js
@@ -4,7 +4,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 const Ready = ({ handleSubmit }) => (
   <div className="slide">
     <div className="slide-header">¡Estamos listos!</div>
-    <div className="slide-body">
+    <div className="row align-center justify-center">
       <RaisedButton
         label="Comenzar"
         backgroundColor="#0091EA"
